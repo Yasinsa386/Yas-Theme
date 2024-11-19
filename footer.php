b4d2a5dbb2c4c3601205bac6352bcfa821edd7f4
Yasin Sa <Yasinsa386@gmail.com>
2024-11-19 17:16:39 +0330

    not work so move to another wp

--- footer.php
+++ footer.php
@@ -1,1 +1,9 @@
-<?php
+<!-- JavaScript -->
+<script src="vendor/leaflet/leaflet.js"></script>
+<script src="js/vendors.js"></script>
+<script src="js/main.js"></script>
+<?php wp_footer(); ?>
+</body>
+
+
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/base.min.css" />
+	<link rel="stylesheet" href="css/choices.min.css" />
+
+	<link rel="stylesheet" href="css/all.min.css"/>
+	<link rel="stylesheet" href="vendor/leaflet/leaflet.css"/>
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/vendors.css">
+	<link rel="stylesheet" href="css/main.css">
+
+	<title>Educrat - قالب HTML دوره آنلاین و آموزش اِدُکرات</title>
+<?php wp_head();?>
+</head>
